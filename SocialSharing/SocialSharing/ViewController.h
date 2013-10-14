2dd175692ca649b415aa07f9106a10c2ec662f3b
Michael Girard <sokkermick@gmail.com>
2013-10-14 15:13:13 -0400

    Now using sharing.

--- SocialSharing/SocialSharing/ViewController.h
+++ SocialSharing/SocialSharing/ViewController.h
@@ -8,13 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UINavigationControllerDelegate, UITextFieldDelegate>
+@interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate>
 
 @property (nonatomic, strong) IBOutlet UIButton *shareBtn;
 @property (nonatomic, strong) IBOutlet UILabel *resultsLabel;
 @property (nonatomic, strong) IBOutlet UITextField *inputText;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView;
 
 - (IBAction)shareButtonClick:(id)sender;
-
+- (IBAction)capturePhoto:(id)sender;
 
 @end
--- SocialSharing/SocialSharing/ViewController.m
+++ SocialSharing/SocialSharing/ViewController.m
@@ -11,15 +11,19 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImage *theImage;
+
 @end
 
 @implementation ViewController
-@synthesize shareBtn, resultsLabel, inputText;
+@synthesize shareBtn, resultsLabel, inputText, imageView, theImage;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(takePicture:)];
+	[self.imageView addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,19 +33,56 @@
 }
 
 - (IBAction)shareButtonClick:(id)sender {
-    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
-        SLComposeViewController *vc = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
-        [vc setInitialText:inputText.text];
-        [self presentViewController:vc animated:YES completion:nil];
-    } else {
-        NSLog(@"facebook is not configured");
-    }
-    
+	NSArray *items = nil;
+	if (theImage) {
+		items = @[theImage, inputText.text];
+	} else {
+		items = @[inputText.text];
+	}
+    UIActivityViewController *shareVc = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
+    [shareVc setCompletionHandler:^(NSString *activityType, BOOL completed) {
+		NSString *outString = [NSString stringWithFormat:@"completed is %d, sharing type is... %@", completed, activityType];
+        resultsLabel.text = outString;
+		if (completed) {
+			inputText.text = @"";
+		}
+    }];
+    [self presentViewController:shareVc animated:YES completion:nil];
 }
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
     return YES;
+}
+
+- (void) takePicture:(UITapGestureRecognizer *)tapRecognizer {
+	UIImagePickerController *imageController = [[UIImagePickerController alloc] init];
+    [imageController setDelegate:self];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        [imageController setSourceType:UIImagePickerControllerSourceTypeCamera];
+    } else {
+        [imageController setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }
+    [self presentViewController:imageController animated:YES completion:nil];
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *inputImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    if (inputImage) {
+        theImage = inputImage;
+		[self refreshUI];
+    }
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) refreshUI {
+    if (theImage) {
+        imageView.image = theImage;
+    }
+}
+
+- (IBAction)capturePhoto:(id)sender {
+	[self takePicture:nil];
 }
 
 @end
